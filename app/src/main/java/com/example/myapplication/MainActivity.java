bb127ba42237b8ae67510ceb0099fabc119f380a
raghav-rama <hackerer528@gmail.com>
2021-10-07 13:46:05 +0530

    dark mode UI revamp

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+//import androidx.cardview.widget.CardView;
 //import androidx.constraintlayout.widget.ConstraintLayout;
-//import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import android.os.Bundle;
 import android.view.View;
@@ -19,13 +20,16 @@ import com.google.android.material.snackbar.Snackbar;
 import java.math.BigDecimal;
 import java.math.MathContext;
 
+import soup.neumorphism.NeumorphCardView;
+
 public class MainActivity extends AppCompatActivity {
     private Button button;
     private TextView textView2;
     private EditText editText;
+    private NeumorphCardView cardView1;
 //    private Snackbar snackbar;
 //     private LinearLayout mainLayout;
-    // private ConstraintLayout mainLayout;
+     private CoordinatorLayout mainLayout;
    private ScrollView scrollview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +38,8 @@ public class MainActivity extends AppCompatActivity {
         button = findViewById(R.id.button);
         textView2 = findViewById(R.id.textView2);
         editText = findViewById(R.id.editText);
-        scrollview = findViewById(R.id.scrollview);
+        mainLayout = findViewById(R.id.mainLayout);
+        cardView1 = findViewById(R.id.cardView1);
         // mainLayout = findViewById(R.id.mainLayout);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,18 +58,31 @@ public class MainActivity extends AppCompatActivity {
                     int precision = POUNDS.toString().indexOf('.') + 2;
                     if (precision > 14) precision = 14;
                     MathContext mc = new MathContext(precision);
-                    textView2.setText("The corresponding value in pounds is " + POUNDS.round(mc));
-                    showSnackbar();
+                    cardView1.setVisibility(View.VISIBLE);
+                    String pounds = POUNDS.round(mc).toString();
+                    int indexOfDot = pounds.indexOf('E');
+                    if(indexOfDot!=-1 && !(pounds.charAt(0)=='0')) {
+//                      System.out.println(pounds);
+                      pounds = pounds.substring(0,indexOfDot) + " x " + "10^" + pounds.substring(indexOfDot+1);
+                      textView2.setText("The corresponding value in pounds is " + pounds);
+                    } else if(indexOfDot==-1 && pounds.charAt(0)=='0') {
+                        textView2.setText("The corresponding value in pounds is " + pounds);
+                    } else if(indexOfDot!=-1 && pounds.charAt(0)=='0') {
+                        textView2.setText("The corresponding value in pounds is 0");
+                    } else {
+                        textView2.setText("The corresponding value in pounds is " + pounds);
+                    }
+//                     showSnackbar();
                 }
             }
         });
     }
     public void showSnackbar() {
-      Snackbar snackbar = Snackbar.make(scrollview, "Who's laughing now?", Snackbar.LENGTH_INDEFINITE)
+      Snackbar snackbar = Snackbar.make(mainLayout, "Who's laughing now?", Snackbar.LENGTH_INDEFINITE)
       .setAction("ME", new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            Snackbar snb1 = Snackbar.make(scrollview, "YOU'RE GODDAMN RIGHT!", Snackbar.LENGTH_SHORT);
+            Snackbar snb1 = Snackbar.make(mainLayout, "YOU'RE GODDAMN RIGHT!", Snackbar.LENGTH_SHORT);
             snb1.show();
         }
       });
